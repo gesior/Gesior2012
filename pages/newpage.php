0f5cb0708d4fe8841263d03ef77a110450a1a334
BroxinDEV <61125744+BroxinDEV@users.noreply.github.com>
2020-06-20 04:47:59 +0200

    create newpage.php
    init

---
+++ pages/newpage.php
@@ -0,0 +1,17 @@
+<?php if(!defined('INITIALIZED')) exit;
+/**
+ * Comment block.
+ *
+ * @file: pages/newpage.php
+ *
+ * @new_page_addr:    ./?subtopic=newpage
+ * @html_link:   <a href="./?subtopic=newpage">NewPage Address</a>
+ */
+
+  $main_content .= '<div class="container"><div class="row"><h3>Put you front-end content here.</h3></div>';
+  
+  
+  $main_content = '<div class="row"><h3>Another line with html content.</h3></div></div>';
+ 
+ 
+  
